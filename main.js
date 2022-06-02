a169bb61959c026d9053781141db2b0381e3f0bd
vijaydev133 <vijaydeveloper004@gmail.com>
2022-06-02 13:22:23 +0530

    Modifying code.

--- main.js
+++ main.js
@@ -4,16 +4,19 @@
 
 let lowNum = 4,
   hiNum = 12;
-
-for (let i = lowNum; i <= hiNum; i++) {
-  let flag = 0;
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) {
-      flag = 1;
-      break;
+if (lowNum > 0 && hiNum > 0) {
+  for (let i = lowNum; i <= hiNum; i++) {
+    let flag = 0;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        flag = 1;
+        break;
+      }
+    }
+    if (i > 1 && flag == 0) {
+      console.log(`the number ${i} is prime number`);
     }
   }
-  if (i > 1 && flag == 0) {
-    console.log(i);
-  }
+} else {
+  console.log("Enter both number as positive number");
 }
